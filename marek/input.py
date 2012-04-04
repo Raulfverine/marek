c6afd2464c777e9e7c55515e1beb33aa05a38150
Anton Berezin <gurunars@gmail.com>
2012-04-04 23:46:06 +0300

    Implemented transformers' concept

--- marek/input.py
+++ marek/input.py
@@ -1,7 +1,10 @@
 from marek import project
 
 
-def get_input(help_message, default=None):
+def get_input(help_message, default=None, transformer=None):
+    """ Gets input from keyboard and processes it """
+    if transformer:
+        default = transformer(default)
     if default is not None and project.quiet:
         return default
     if default is not None:
@@ -12,4 +15,9 @@ def get_input(help_message, default=None):
     while not data and default is None:
         print "There is not default for this placeholder. Please enter some value or cancel the creation process."
         data = raw_input("[mandatory] %s" % help_message)
+    if transformer and data:
+        trans_data = transformer(data)
+        if data != trans_data:
+            print "Your input was transformed from '%s' to '%s'" % (data, trans_data)
+        data = trans_data
     return data or default
--- templates/simple_deb/.rules.py
+++ templates/simple_deb/.rules.py
@@ -1,9 +1,10 @@
 import os
 from marek.input import get_input
 from marek import project
+from marek.transformers import debianize
 
 data = {
-   "deb_name": get_input("Debian package name", project.name.lower().replace(" ", "-").replace("_", "-")),
+   "deb_name": get_input("Debian package name", project.name, debianize),
    "deb_maintainer": get_input("Debian maintainer", os.environ.get("DEBFULLNAME", None)),
    "deb_email": get_input("Debian email", os.environ.get("DEBEMAIL", None)),
    "deb_description": get_input("Debian description")
