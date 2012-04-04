0b130b2a29c464466a3b41ab225f002c83477162
Anton Berezin <gurunars@gmail.com>
2012-04-04 22:40:49 +0300

    Fixed input message if the default value is missing.

--- marek/input.py
+++ marek/input.py
@@ -9,6 +9,8 @@ def get_input(help_message, default=None):
         return default
     if default is not None:
         help_message = "%s [%s]: " % (help_message, str(default))
+    else:
+        help_message = "%s: " % help_message
     data = raw_input(help_message)
     if not data and default is None:
         raise NoValueError("Value is missing.")
