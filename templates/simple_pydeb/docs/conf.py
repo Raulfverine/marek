d44476bd1842fedfd28daec86572780827eee7ae
Anton Berezin <anton.berezin@f-secure.com>
2012-04-10 16:23:52 +0300

    Added docs folder to the template.

---
+++ templates/simple_pydeb/docs/conf.py
@@ -0,0 +1,84 @@
+import sys, os
+
+# -- General configuration -----------------------------------------------------
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['.templates']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The encoding of source files.
+#source_encoding = 'utf-8'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'%{python_name}'
+copyright = u'2012, F-Secure'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = open("../debian/changelog").readline().split(' ')[1][1:-1]
+# The full version, including alpha/beta/rc tags.
+release = version
+
+# List of directories, relative to source directory, that shouldn't be searched
+# for source files.
+exclude_trees = ['.build']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+
+# -- Options for HTML output ---------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+html_theme = 'default'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
+
+# Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = []
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+#html_title = None
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+#html_short_title = None
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#html_logo = None
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['.static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = '%{python_name}doc'
+
+# Theming
+html_theme = "default"
+html_theme_options = {
+    "rightsidebar": "true",
+    "stickysidebar": "true",
+    "sidebarbgcolor": "#696969",
+    "relbarbgcolor": "#000000"
+}
+
---
+++ templates/simple_pydeb/docs/index.rst
@@ -0,0 +1,13 @@
+%{python_name} documentation
+============================
+
+cheat sheet : http://docutils.sourceforge.net/docs/user/rst/quickref.html
+
+Contents:
+
+.. toctree::
+   :maxdepth: 3
+
+   one
+   two
+   three
